9dc3c33feca67ef5df4ea9b32be8ece0c8071788
Erik Gilling <konkers@konkers.net>
2016-09-17 22:24:42 -0700

    sandbox: Add list.h.

---
+++ sandbox/list.h
@@ -0,0 +1,90 @@
+#ifndef SANDBOX_LIST_H_
+#define SANDBOX_LIST_H_
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+// TODO(konkers): This really belongs somewhere else.
+#define container_of(p, type, member) \
+    ((type *)((uint8_t *)(p) - offsetof(type, member)))
+
+struct list_head {
+    struct list_head *next;
+    struct list_head *prev;
+};
+
+static inline void list_init(struct list_head *list) {
+    list->next = list;
+    list->prev = list;
+}
+
+static inline void list_insert_after(struct list_head *cur_elem,
+                                     struct list_head *new_elem) {
+    new_elem->next = cur_elem->next;
+    new_elem->prev = cur_elem;
+    cur_elem->next->prev = new_elem;
+    cur_elem->next = new_elem;
+}
+
+static inline void list_insert_before(struct list_head *cur_elem,
+                                     struct list_head *new_elem) {
+    new_elem->next = cur_elem;
+    new_elem->prev = cur_elem->prev;
+    cur_elem->prev->next = new_elem;
+    cur_elem->prev = new_elem;
+}
+
+static inline void list_push_front(struct list_head *list,
+                                   struct list_head *elem) {
+    list_insert_after(list, elem);
+}
+
+static inline void list_push_back(struct list_head *list,
+                                  struct list_head *elem) {
+    list_insert_before(list, elem);
+}
+
+static inline struct list_head *list_front(struct list_head *list) {
+    return list->next;
+}
+
+static inline struct list_head *list_back(struct list_head *list) {
+    return list->prev;
+}
+
+static inline bool list_is_end(struct list_head *list, struct list_head *elem) {
+    return list == elem;
+}
+
+#define list_container(list, type, member) \
+    container_of(list, type, member)
+
+#define list_container_next(elem, member) \
+    container_of(elem->member.next, typeof(*elem), member)
+
+#define list_container_prev(elem, member) \
+    container_of(elem->member.prev, typeof(*elem), member)
+
+#define list_foreach(list, elem)    \
+    for (elem = list_front((list)); \
+         !list_is_end(list, elem);  \
+         elem = elem->next)
+
+#define list_foreach_rev(list, elem) \
+    for (elem = list_back((list));   \
+         !list_is_end(list, elem);   \
+         elem = elem->prev)
+
+#define list_foreach_container(list, elem, member)                       \
+    for (elem = list_container(list_front(list), typeof(*elem), member); \
+         !list_is_end(list, &elem->member);                              \
+         elem = list_container_next(elem, member))
+
+#define list_foreach_container_rev(list, elem, member)                  \
+    for (elem = list_container(list_back(list), typeof(*elem), member); \
+         !list_is_end(list, &elem->member);                             \
+         elem = list_container_prev(elem, member))
+
+#endif // SANDBOX_LIST_H_
+
---
+++ sandbox/list_test.c
@@ -0,0 +1,133 @@
+#include <external/unity/src/unity.h>
+#include <sandbox/list.h>
+
+struct test_elem {
+    struct list_head list;
+    uint32_t val;
+};
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+static void test_list_add_front(void) {
+    struct list_head list;
+    struct test_elem elem0 = {.val = 0};
+    struct test_elem elem1 = {.val = 1};
+    struct test_elem elem2 = {.val = 2};
+    struct test_elem elem3 = {.val = 3};
+
+    list_init(&list);
+    list_push_front(&list, &elem0.list);
+    list_push_front(&list, &elem1.list);
+    list_push_front(&list, &elem2.list);
+    list_push_front(&list, &elem3.list);
+
+    uint32_t val = 3;
+    struct list_head *e;
+    list_foreach(&list, e) {
+        struct test_elem *elem;
+        elem = container_of(e, struct test_elem, list);
+        TEST_ASSERT_EQUAL_UINT32(val, elem->val);
+        val--;
+    }
+
+    val = 0;
+    list_foreach_rev(&list, e) {
+        struct test_elem *elem;
+        elem = container_of(e, struct test_elem, list);
+        TEST_ASSERT_EQUAL_UINT32(val, elem->val);
+        val++;
+    }
+    TEST_ASSERT_EQUAL_UINT32(4, val);
+}
+
+static void test_list_add_back(void) {
+    struct list_head list;
+    struct test_elem elem0 = {.val = 0};
+    struct test_elem elem1 = {.val = 1};
+    struct test_elem elem2 = {.val = 2};
+    struct test_elem elem3 = {.val = 3};
+
+    list_init(&list);
+    list_push_back(&list, &elem0.list);
+    list_push_back(&list, &elem1.list);
+    list_push_back(&list, &elem2.list);
+    list_push_back(&list, &elem3.list);
+
+    uint32_t val = 0;
+    struct list_head *e;
+    list_foreach(&list, e) {
+        struct test_elem *elem;
+        elem = container_of(e, struct test_elem, list);
+        TEST_ASSERT_EQUAL_UINT32(val, elem->val);
+        val++;
+    }
+    TEST_ASSERT_EQUAL_UINT32(4, val);
+
+    val = 3;
+    list_foreach_rev(&list, e) {
+        struct test_elem *elem;
+        elem = container_of(e, struct test_elem, list);
+        TEST_ASSERT_EQUAL_UINT32(val, elem->val);
+        val--;
+    }
+    TEST_ASSERT_EQUAL_UINT32(0 - 1, val);
+
+    val = 0;
+    struct test_elem *elem;
+    list_foreach_container(&list, elem, list) {
+        TEST_ASSERT_EQUAL_UINT32(val, elem->val);
+        val++;
+    }
+    TEST_ASSERT_EQUAL_UINT32(4, val);
+
+    val = 3;
+    list_foreach_container_rev(&list, elem, list) {
+        TEST_ASSERT_EQUAL_UINT32(val, elem->val);
+        val--;
+    }
+    TEST_ASSERT_EQUAL_UINT32(0 - 1, val);
+}
+
+static void test_list_insert(void) {
+    struct list_head list;
+    struct test_elem elem0 = {.val = 0};
+    struct test_elem elem1 = {.val = 1};
+    struct test_elem elem2 = {.val = 2};
+    struct test_elem elem3 = {.val = 3};
+
+    list_init(&list);
+    list_push_back(&list, &elem0.list);
+    list_push_back(&list, &elem3.list);
+    list_insert_after(&elem0.list, &elem1.list);
+    list_insert_before(&elem3.list, &elem2.list);
+
+
+    uint32_t val = 0;
+    struct test_elem *elem;
+    list_foreach_container(&list, elem, list) {
+        TEST_ASSERT_EQUAL_UINT32(val, elem->val);
+        val++;
+    }
+    TEST_ASSERT_EQUAL_UINT32(4, val);
+
+    val = 3;
+    list_foreach_container_rev(&list, elem, list) {
+        TEST_ASSERT_EQUAL_UINT32(val, elem->val);
+        val--;
+    }
+    TEST_ASSERT_EQUAL_UINT32(0 - 1, val);
+}
+
+int main(int argc, char *argv[]) {
+    UNITY_BEGIN();
+
+    RUN_TEST(test_list_add_front);
+    RUN_TEST(test_list_add_back);
+    RUN_TEST(test_list_insert);
+
+    return UNITY_END();
+}
